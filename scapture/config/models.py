fa870df687411511df804079804bb1adb3c7d24d
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2023-11-18 14:43:46 +0900

    feat : S3 연결 (#1)

--- scapture/config/models.py
+++ scapture/config/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Video(models.Model):
     game = models.PositiveBigIntegerField(null=True, blank=True)
     create_at = models.DateTimeField(null=True, blank=True)
-    video = models.FileField()
+    video = models.FileField(upload_to='videos/')
