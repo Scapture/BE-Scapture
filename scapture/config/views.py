c03e4ae0cdfea489af1899f3d835c424afa4c12d
xogk1128 <xogk1128@naver.com>
2024-02-04 19:30:19 +0900

    feat: 특정 id에 대한 영상 삭제하기

--- scapture/config/views.py
+++ scapture/config/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Video
 from .serializers import VideoSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 class VideoListAPIView(generics.ListAPIView):
@@ -15,6 +17,16 @@ class VideoListAPIView(generics.ListAPIView):
 class VideoCreateAPIView(generics.CreateAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+
+class VideoDestroyAPIView(generics.DestroyAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()  # 특정 ID에 해당하는 객체 가져오기
+        self.perform_destroy(instance)  # 객체 삭제
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 # class GameListAPIView(generics.ListAPIView):
 #     serializer_class = VideoSerializer
