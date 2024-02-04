b2630be1ade75acd1736491f89449f18ef2146aa
xogk1128 <xogk1128@naver.com>
2024-02-04 19:07:00 +0900

    feat: 파일 이름 지정하여 저장하기

--- scapture/config/models.py
+++ scapture/config/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
+def generate_video_filename(instance, filename):
+    # filename에서 확장자 추출
+    extension = filename.split('.')[-1]
+    
+    # 동영상 파일 이름 생성 (예: video1.mov, video2.mov, ...)
+    new_filename = f'video{instance.id}.{extension}'
+    
+    # 저장 경로 설정 (S3 또는 다른 스토리지에 따라 설정이 달라질 수 있음)
+    return f'videos/{new_filename}'
+
 # Create your models here.
 class Video(models.Model):
     game = models.PositiveBigIntegerField(null=True, blank=True)
     create_at = models.DateTimeField(null=True, blank=True)
-    video = models.FileField(upload_to='videos/')
+    video = models.FileField(upload_to=generate_video_filename)
